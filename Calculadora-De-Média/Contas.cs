826ab864a3d3fc4bc6856bb2394ff805beae2be9
Caique Coelho Pinheiro <111666245+0246808642@users.noreply.github.com>
2022-12-19 15:22:13 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora-De-Média/Contas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+namespace Calculadora_De_Média
+{
+    internal class Contas
+    {
+        public double AV1 { get; set; }
+        public double  AV2 { get; set; }
+        public double Tab { get; set; }
+        
+
+        public Contas(double aV1, double aV2, double tab)
+        {
+            AV1 = aV1;
+            AV2 = aV2;
+            Tab = tab;
+
+        }
+
+        public double Media()
+        {
+            return (AV1 + AV2 + Tab) /3;
+        }
+    }
+
+
+
+
+}
---
+++ Calculadora-De-Média/Program.cs
@@ -0,0 +1,16 @@
+﻿using Calculadora_De_Média;
+using System;
+using System.Globalization;
+
+
+Console.WriteLine("Digite um sua nota (AV1): ");
+double aV1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+Console.WriteLine("Digite um sua nota (AV2): ");
+double av2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite a nota do seu trabalho: ");
+double tab = double.Parse(Console.ReadLine());
+
+
+Contas C = new Contas(aV1,av2,tab);
+
+Console.WriteLine("Sua Média é " + C.Media().ToString ("F2"), CultureInfo.InvariantCulture);
